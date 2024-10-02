208a4b76bab7466ea5fdcf1942e547092ee7272d
tymer007 <ty.freeman47@gmail.com>
2024-10-02 03:19:10 +0100

    destroyed cloudinary

--- controllers/markerController.js
+++ controllers/markerController.js
@@ -20,19 +20,6 @@ export const createMarker = async (req, res) => {
   }
 
   try {
-    // Upload cover photo to Cloudinary if provided
-    if (marker.coverPhoto) {
-      const uploadedResponse = await cloudinary.uploader.upload(marker.coverPhoto, {
-        resource_type: 'auto',
-    });
-      marker.coverPhoto = uploadedResponse.secure_url;
-    }
-  } catch (error) {
-    console.error("Error in uploading cover photo:", error.message || error);
-    return res.status(500).json({ success: false, message: "Error uploading cover photo" });
-  }
-
-  try {
     const newMarker = new Marker(marker);
     await newMarker.save();
     res.status(201).json({ success: true, data: newMarker });
@@ -47,27 +34,41 @@ export const updateMarker = async (req, res) => {
   const marker = req.body;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ success: false, message: "Invalid marker ID" });
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid marker ID" });
   }
 
   try {
     // Destroy the old cover photo in Cloudinary if updating
     if (marker.coverPhoto) {
-      await cloudinary.uploader.destroy(marker.coverPhoto.split("/").pop().split(".")[0]);
-      
+      await cloudinary.uploader.destroy(
+        marker.coverPhoto.split("/").pop().split(".")[0]
+      );
+
       // Upload new cover photo
-      const uploadedResponse = await cloudinary.uploader.upload(marker.coverPhoto, {
-        resource_type: 'auto',
-    });
+      const uploadedResponse = await cloudinary.uploader.upload(
+        marker.coverPhoto,
+        {
+          resource_type: "auto",
+        }
+      );
       marker.coverPhoto = uploadedResponse.secure_url;
     }
   } catch (error) {
-    console.error("Error in handling cover photo update:", error.message || error);
-    return res.status(500).json({ success: false, message: "Error updating cover photo" });
+    console.error(
+      "Error in handling cover photo update:",
+      error.message || error
+    );
+    return res
+      .status(500)
+      .json({ success: false, message: "Error updating cover photo" });
   }
 
   try {
-    const updatedMarker = await Marker.findByIdAndUpdate(id, marker, { new: true });
+    const updatedMarker = await Marker.findByIdAndUpdate(id, marker, {
+      new: true,
+    });
     res.status(200).json({ success: true, data: updatedMarker });
   } catch (error) {
     console.error("Error in updating marker:", error.message || error);
@@ -79,23 +80,31 @@ export const deleteMarker = async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ success: false, message: "Invalid marker ID" });
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid marker ID" });
   }
 
   try {
     const marker = await Marker.findById(id);
 
     if (!marker) {
-      return res.status(404).json({ success: false, message: "Marker not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Marker not found" });
     }
 
     // Destroy cover photo in Cloudinary if it exists
     if (marker.coverPhoto) {
-      await cloudinary.uploader.destroy(marker.coverPhoto.split("/").pop().split(".")[0]);
+      await cloudinary.uploader.destroy(
+        marker.coverPhoto.split("/").pop().split(".")[0]
+      );
     }
 
     await Marker.findByIdAndDelete(id);
-    res.status(200).json({ success: true, message: "Marker deleted successfully" });
+    res
+      .status(200)
+      .json({ success: true, message: "Marker deleted successfully" });
   } catch (error) {
     console.error("Error in deleting marker:", error.message || error);
     res.status(500).json({ success: false, message: "Server error" });
