4deca93c44fa3799a54af526cba44976f026e193
tymer007 <ty.freeman47@gmail.com>
2025-03-15 12:13:15 +0100

    models

--- models/markerModel.js
+++ models/markerModel.js
@@ -19,7 +19,8 @@ const MarkerSchema = new mongoose.Schema({
   },
   buildingName: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   description: {
     type: String,
--- models/userModel.js
+++ models/userModel.js
@@ -10,6 +10,7 @@ const UserSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true
     },
     password: {
       type: String,
